10d8e06a4df19b15a488f3beeafa98dd964fe27f
Yuliya <juliankawonder@gmail.com>
2018-10-18 19:55:46 +0300

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
+// App Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
@@ -85,7 +90,9 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 	    MatPaginatorModule,
 
 		FormsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
 	],
 	providers: [
 	// no need to place any providers due to the `providedIn` flag...
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
  	providedIn: 'root',
 })
 export class HeroService {
+	private heroesUrl = 'api/heroes';
 
-	constructor(private messageService: MessageService) { }
+	constructor(
+		private http: HttpClient,
+		private messageService: MessageService
+		) { }
 
-	getHeroes(): Observable<Hero[]> {
-		// TODO: send the message _after_ fetching the heroes
-		this.messageService.add('HeroService: fetched heroes');
-		return of(HEROES);
+	private log(message: string) {
+		this.messageService.add(`HeroService: ${message}`);
 	}
 
-	getHero(id: number): Observable<Hero> {
+	getHeroes(): Observable<Hero[]> {
+		return this.http.get<Hero[]>(this.heroesUrl);
+	}
+
+	/*getHero(id: number): Observable<Hero> {
 		// TODO: send the message _after_ fetching the hero
 		this.messageService.add(`HeroService: fetched hero id=${id}`);
 		return of(HEROES.find(hero => hero.id === id));
-	}
+	}*/
 }
